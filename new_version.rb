ff66ad9db5b1a31546812d96ac300adce94e4c50
Haritha Denuwan <110480085+denxwan@users.noreply.github.com>
2025-10-07 15:29:49 +1100

    Update new_version.rb

--- new_version.rb
+++ new_version.rb
@@ -98,11 +98,13 @@ end
 
 class MazeCreation < Gosu::Window
   def initialize 
-    super WIDTH, HEIGHT, ROWS, COLS
+    super(WIDTH, HEIGHT, false)
+    # super WIDTH, HEIGHT, ROWS, COLS
     self.caption = "Maze Creation"
-    @should_draw_ruby = false
+    # @should_draw_ruby = false
     @sel_x = nil
     @sel_y = nil
+    @cell_state = nil
     @cells = Array.new()
 
     # Matrix initialization
@@ -151,7 +153,9 @@ class MazeCreation < Gosu::Window
 
   def draw
     # @background.draw(0, 0, ZOrder::BACKGROUND)
-    draw_rect(0, 0, self.width, self.height, Gosu::Color.new(122, 251, 64))
+    
+    #draw_rect(0, 0, self.width, self.height, Gosu::Color.new(122, 251, 64))
+    #@cells.each { |cell| draw_cell(cell) }
     # for i in 0..9
     #   for j in 0..9
     #     draw_rect(i, j, 50, 50, Gosu::Color.new(122, 251, 64))
@@ -164,7 +168,18 @@ class MazeCreation < Gosu::Window
     #   # insert()
     #   draw_rect(@sel_x*50, @sel_y*50, 50, 50, Gosu::Color.new(254, 255, 75))
     # end
-    @cells.each { |cell| draw_cell(cell) }
+
+    # Matrix method for draw func
+    for i in 0..9
+      for j in 0..9
+        if (Cell_Matrix[i][j] == true)
+          draw_rect(i*50, j*50, 50, 50, Gosu::Color.new(254, 255, 75))
+        else
+          draw_rect(i*50, j*50, 50, 50, Gosu::Color.new(122, 251, 64))
+        end
+      end
+    end
+
   end
 
   def button_down(id)
@@ -178,42 +193,112 @@ class MazeCreation < Gosu::Window
   end
 
   def draw_cell cell
-    if (Cell_Matrix[@sel_x][@sel_y] == false)
-      draw_rect(
-        @sel_x*50,
-        @sel_y*50,
-        50,
-        50,
-        Gosu::Color.new(254, 255, 75)
-    )
-    else if (Cell_Matrix[@sel_x][@sel_y] == true)
-      draw_rect(
-        @sel_x*50,
-        @sel_y*50,
-        50,
-        50,
-        Gosu::Color.new(122, 251, 64)
-    )
+    # Default color of the cell
+    color = Gosu::Color.new(254, 255, 75)
+    if (@cell_state == true)
+      color = Gosu::Color.new(122, 251, 64)
+    else
+      color = Gosu::Color.new(254, 255, 75)
     end
-    end
+
+    draw_rect(
+      @sel_x*50,
+      @sel_y*50,
+      50,
+      50,
+      color = Gosu::Color.new(122, 251, 64)
+    )
+    # if (Cell_Matrix[@sel_x][@sel_y] == false)
+    #   draw_rect(
+    #     @sel_x*50,
+    #     @sel_y*50,
+    #     50,
+    #     50,
+    #     Gosu::Color.new(254, 255, 75)
+    # )
+    # else if (Cell_Matrix[@sel_x][@sel_y] == true)
+    #   draw_rect(
+    #     @sel_x*50,
+    #     @sel_y*50,
+    #     50,
+    #     50,
+    #     Gosu::Color.new(122, 251, 64)
+    # )
+    # end
+    # end
   end
 
   def GetPosition(mouse_value_x, mouse_value_y)
-    temp_x = (mouse_value_x-20).div(60)
-    temp_y = mouse_value_y.div(60)
+    temp_x = (mouse_value_x).div(50)
+    temp_y = mouse_value_y.div(50)
     @sel_x = temp_x
     @sel_y = temp_y
 
     
 
-    puts Cell_Matrix[@sel_x][@sel_y]
+    #puts Cell_Matrix[@sel_x][@sel_y]
     # puts "#{mouse_value_x-20} #{mouse_value_y}"
     # puts "#{@sel_x} #{@sel_y}"
 
-    @cells.push(Cell.new(@sel_x, @sel_y))
-
     # Inversing the state
-    Cell_Matrix[@sel_x][@sel_y] = !Cell_Matrix[@sel_x][@sel_y]
+    # Cell_Matrix[@sel_x][@sel_y] = !Cell_Matrix[@sel_x][@sel_y]
+
+    if (Cell_Matrix[@sel_x][@sel_y] == false)
+      Cell_Matrix[@sel_x][@sel_y] = true
+      @cell_state = Cell_Matrix[@sel_x][@sel_y]
+
+    else
+      Cell_Matrix[@sel_x][@sel_y] = false
+      @cell_state = Cell_Matrix[@sel_x][@sel_y]
+    end
+
+    # @cells.push(Cell.new(@sel_x, @sel_y))
+    #puts "Cell #{@sel_x} #{@sel_y} has been changed to -> #{Cell_Matrix[@sel_x][@sel_y]}"
+
+    for i in 0..9
+      for j in 0..9
+        north = 0
+        south = 0
+        east = 0
+        west = 0
+
+        # Check north
+        temp_j = j-1
+        if ((temp_j >= 0) && (temp_j < 9))
+          if (Cell_Matrix[i][temp_j] == true)
+            north = 1
+          end
+        end
+
+        # Check south
+        temp_j = j+1
+        if ((temp_j >= 0) && (temp_j < 9))
+          if (Cell_Matrix[i][temp_j] == true)
+            south = 1
+          end
+        end
+
+        # Check east
+        temp_i = i+1
+        if ((temp_i >= 0) && (temp_i < 9))
+          if (Cell_Matrix[temp_i][j] == true)
+            east = 1
+          end
+        end
+
+        # Check west
+        temp_i = i-1
+        if ((temp_i >= 0) && (temp_i < 9))
+          if (Cell_Matrix[temp_i][j] == true)
+            west = 1
+          end
+        end
+
+        puts "Cell x: #{i}, y: #{j} north:#{north} south:#{south} east:#{east} west:#{west}"
+      end
+      puts "------------------- End of Column -------------------"
+    end
+
 
     # # Change state to active
     # if (Cell_Matrix[temp_x][temp_y] == false)
@@ -230,4 +315,6 @@ class MazeCreation < Gosu::Window
   end
 end
 
-MazeCreation.new.show
+# MazeCreation.new.show
+window = MazeCreation.new
+window.show
